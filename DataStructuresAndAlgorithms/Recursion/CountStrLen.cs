a0bee10cc312378d1c2f64543f12990258431b21
Manasjyoti Borah <manasborah90@gmail.com>
2021-09-03 07:14:25 +0530

    recursion

---
+++ DataStructuresAndAlgorithms/Recursion/CountStrLen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms.Recursion
+{
+    class CountStrLen
+    {
+        public static int RCount(string str,int i)
+        {
+            if (i == str.Length)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1 + RCount(str, i + 1);
+            }
+        }
+
+        public static void Count(string str)
+        {
+            Console.WriteLine("'{0}' has a length of {1}", str, RCount(str,0));
+        }
+
+        static void Main()
+        {
+            Count("hello world");
+            Count("apple");
+            Count("toy");
+            Count("");
+
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Recursion/ReverseString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms.Recursion
+{
+    class ReverseString
+    {
+        public static string RReverse(string str, int i)
+        {
+            if (i == str.Length)
+            {
+                return "";
+            }
+            else
+            {
+                return RReverse(str,i+1) + str[i];
+            }
+        }
+
+        public static void Reverse(string str)
+        {
+            Console.WriteLine("'{0}' reversed is '{1}'", str, RReverse(str, 0));
+        }
+
+        static void Main()
+        {
+            Reverse("hello world");
+            Reverse("apple");
+            Reverse("toy");
+            Reverse("");
+
+        }
+    }
+}
