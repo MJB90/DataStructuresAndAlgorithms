700f8d5fe34787ff0755f7321c53d44edcaac979
Manasjyoti Borah <manasborah90@gmail.com>
2021-09-07 11:49:12 +0530

    dictionary

---
+++ DataStructuresAndAlgorithms/Dictionary/q1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms.Dictionary
+{
+    class q1
+    {
+        public static bool CheckSameSet(int[] a, int[] b)
+        {
+            bool sameSet = true;
+            int i;
+            Dictionary<int, int> aa = new Dictionary<int, int>();
+            Dictionary<int, int> bb = new Dictionary<int, int>();
+
+            for (i = 0; i < a.Length; i++)
+            {
+                if (aa.ContainsKey(a[i])) aa[a[i]]++;
+                else aa[a[i]] = 1;
+            }
+
+            for (i = 0; i < b.Length; i++)
+            {
+                if (bb.ContainsKey(b[i])) bb[b[i]]++;
+                else bb[b[i]] = 1;
+            }
+
+            foreach(int k in aa.Keys)
+            {
+                if (bb.ContainsKey(k)==false || aa[k] != bb[k])
+                {
+                    sameSet = false;
+                    break;
+                }
+                    
+            }
+            return sameSet;
+        }
+
+        static void Main()
+        {
+            int[] a = { 2,4, 2, 4 ,3};
+            int[] b = { 4,2,3,4,2 };
+
+            Console.WriteLine(CheckSameSet(a, b));
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Dictionary/q2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms.Dictionary
+{
+    class q2
+    {
+        public static void PrintPairs(int[] a)
+        {
+            Dictionary<int, int> elements = new Dictionary<int, int>();
+            int i;
+
+            for (i = 0; i < a.Length; i++) elements[a[i]] = 1;
+
+            for (i = 0; i < a.Length; i++)
+            {
+                if (elements.ContainsKey(10 - a[i]) && elements[a[i]]!=0 && a[i]!=10-a[i])
+                {
+                    elements[10 - a[i]] = 0;
+                    Console.Write("{" + a[i] + "," + (10 - a[i]) + "},");
+                }
+            }
+        }
+        static void Main()
+        {
+            int[] a = { 1, 3, 2, 10, 4, 9, 7, 15, 6, 5 };
+
+            PrintPairs(a);
+        }
+    }
+}
